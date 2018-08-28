2420fc23aad0444c056596a59e718fd5a9f6fe79
Hadiissa149 <Hadiissa149@gmail.com>
2018-08-28 15:50:45 +1000

    Added an Icon.

--- Dragon Hatcher/Assets/Scripts/GameManagerScript.cs
+++ Dragon Hatcher/Assets/Scripts/GameManagerScript.cs
@@ -9,15 +9,25 @@ public class GameManagerScript : MonoBehaviour {
 
     // Use this for initialization
     public float moneyCurrent;
-    public int Level1Current;
+    
     public Text moneyIndicator;
+
+    public int Level1Current;
     public Text Level1CostIndicator;
     public float Level1Cost;
     public float Level1XPosition;
 
+    public int Level2Current;
+    public Text Level2CostIndicator;
+    public float Level2Cost;
+    public float Level2XPosition;
+
     public GameObject Level1Icon;
 	void Start () {
         Level1Cost = 10;
+        instance = this;
+
+        Level2Cost = 20;
         instance = this;
 		
 	}
@@ -25,8 +35,12 @@ public class GameManagerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         moneyIndicator.text = "" + (int)moneyCurrent;
-        Level1CostIndicator.text = "" + Level1Cost;
+        
         moneyCurrent += 0.0165f * Level1Current;
+        moneyCurrent += 1.0165f * Level2Current;
+
+        Level1CostIndicator.text = "" + Level1Cost;
+        Level2CostIndicator.text = "" + Level2Cost;
 	}
 
     public void GetMoney()
@@ -46,6 +60,20 @@ public class GameManagerScript : MonoBehaviour {
 
             Instantiate(Level1Icon, new Vector3(Level1XPosition, 0, 0), Quaternion.identity);
             Level1XPosition += 0.5f; 
+        }
+    }
+
+    public void GetLevel2()
+    {
+        if (moneyCurrent >= Level2Cost)
+
+        {
+            Level2Current += 1;
+            moneyCurrent -= Level2Cost;
+            Level2Cost = Level2Cost * 2.15f;
+
+            Instantiate(Level1Icon, new Vector3(Level1XPosition, 0, 0), Quaternion.identity);
+            Level1XPosition += 0.5f;
         }
     }
 
